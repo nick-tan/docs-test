10a983a51b450f0327363b8ead3072f50b5cc76d
nick <nick.tan.orz@gmail.com>
2023-12-18 22:21:56 +0800

    feat:init

---
+++ .vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    title: 'TypeScript4 文档',
+    description: 'TypeScript4 最新官方文档翻译',
+    themeConfig: {
+        nav: [
+            { text: '首页', link: '/' },
+            { 
+                text: 'JavaScript', 
+                items: [
+                    { text: 'Github', link: 'https://github.com/mqyqingfeng' },
+                    { text: '掘金', link: 'https://juejin.cn/user/712139234359182/posts' }
+                ]
+            }
+        ]
+    }
+  }
